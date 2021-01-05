6115ea8ec1003211e80b536f20313cdf5b5368a9
Alexander Maximkin <maximkin.alek@yandex.ru>
2021-01-05 20:41:42 +0300

    all popups work

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,42 +1,23 @@
 
 
-function Main() {
+function Main(props) {
 
-    function handleAvatarClick() {
-        const popupAvatar = document.querySelector('.popup-avatar');
-        popupAvatar.classList.add('popup_is-opened');
-    }
-
-    function handleEditProfileClick() {
-        const popupEdit = document.querySelector('.popup-edit-card');
-        popupEdit.classList.add('popup_is-opened');
-    }
-
-    function handleAddPlaceClick() {
-        const popupAdd = document.querySelector('.popup-add-card');
-        popupAdd.classList.add('popup_is-opened');
-    }
 
     return (
         <>
             <div className="profile root__section">
                 <div className="user-info">
-                    <div className="user-info__photo" onClick={handleAvatarClick} ></div>
+                    <div className="user-info__photo" onClick={props.onEditAvatar} ></div>
                     <div className="user-info__data">
                         <h1 className="user-info__name">Alex</h1>
                         <p className="user-info__job">Developer</p>
-                        <button className="user-info__edit-button" onClick={handleEditProfileClick} >Edit</button>
+                        <button className="user-info__edit-button" onClick={props.onEditProfile} >Edit</button>
                     </div>
-                    <button className="button user-info__button" onClick={handleAddPlaceClick} >+</button>
+                    <button className="button user-info__button" onClick={props.onAddPlace} >+</button>
                 </div>
             </div>
             <div className="places-list root__section">
             </div>
-           
-            
-
-            
-
             
         </>
     );
